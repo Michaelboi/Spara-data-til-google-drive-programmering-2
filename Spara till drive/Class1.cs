900095f92533f10707dec943dd8001dc78e84c44
Michaelboi <micnwa050628@student.jenseneducation.se>
2023-06-02 18:18:15 +0200

    Add project files.

---
+++ Spara till drive/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/*public class Class1
+{
+    public float timer = 1f;
+    public string filepath = "GDrive.csv";
+
+    public void Spara()
+    {
+        File.WriteAllText(filepath, timer.ToString());
+        Console.WriteLine("");
+        Spara();
+    }
+}
+*/
---
+++ Spara till drive/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Xml.Linq;
+
+namespace test
+{
+    public class Program
+    {
+        
+
+        static void Main(string[]args)
+        {
+            float timer = 0;
+            string filepath = "G:\\Min enhet\\sparanu\\GDrive.csv";
+            File.WriteAllText(filepath, timer.ToString());
+            Console.WriteLine("");
+            
+        }
+
+
+
+
+    }
+}
